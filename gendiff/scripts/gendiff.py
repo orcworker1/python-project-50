daf28c8d39c1459ac0fe62d604896022fc847feb
Валентин Алтабасов <valenjir@gmail.com>
2025-04-26 20:48:23 +0300

    fix gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,10 +1,7 @@
 from gendiff.cli import parse_args
-from gendiff.parse import give_data
 
-def generate_diff(file1_path, file2_path):
-    data1 = give_data(file1_path)
-    data2 = give_data(file2_path)
-    return {'file1': data1, 'file2': data2}
+from gendiff.diff import generate_diff
+
 
 
 def main():

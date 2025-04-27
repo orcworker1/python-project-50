40346461d978679d28ff13a42ae93a62b1dac457
Валентин Алтабасов <valenjir@gmail.com>
2025-04-27 16:01:30 +0300

    update test

---
+++ tests/test_diff.py
@@ -0,0 +1,41 @@
+import pytest
+from gendiff import generate_diff
+
+
+@pytest.fixture
+def data_1():
+    return 'tests/fixture/file1.json'
+
+@pytest.fixture
+def data_2():
+    return 'tests/fixture/file2.json'
+
+
+def test_flat_json_diff(data_1, data_2):
+    expected = """{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}"""
+    assert generate_diff(data_1,data_2) == expected
+
+def test_parse_file(data_1):
+    assert generate_diff(data_1, data_1) == """{
+    host: hexlet.io
+    timeout: 50
+    proxy: 123.234.53.22
+    follow: false
+}"""
+
+
+
+
+
+
+
+
+
+
